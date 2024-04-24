43cf5b9bddbf013a435a5bf822301b47b93f57f6
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-04-24 15:13:32 +0200

    feat: update styles for better accessibility and looks
    - reverted to docusaurus header
    - removed custom stylings that hurt accessibility
    - removed unnecessary !important from css custom props
    - minified svgs
    - cleaned up docusaurus config

--- src/components/Layout/AccentGradient.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./layout.module.css";
-
-export default function AccentGradient() {
-  return <div width="100%" height="100%" className={styles.accentGradient} />;
-}
