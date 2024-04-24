c6da9b55349db8b3eed33d41857dcc67fbb94b1f
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-04-24 15:01:00 +0200

    refactor: remove unused styling and markdown page from pages/
    - changed file extension from .js to .jsx for react component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Redirect } from "@docusaurus/router";
 
-const Home = () => {
+export default function Home() {
   return <Redirect to="/intro" />;
-};
-
-export default Home;
+}
--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
+import Layout from "../components/Layout";
 
-// Default implementation, that you can customize
 export default function Root({ children }) {
   return <Layout>{children}</Layout>;
 }
