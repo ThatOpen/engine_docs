9a6ac2a050c1b930b1b786968f1a69c0fe0a38e5
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-06-12 00:19:32 +0200

    feat: make dark the default mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,6 +68,9 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+    },
     // Replace with your project's social card
     image: 'img/logo.svg',
     navbar: {
