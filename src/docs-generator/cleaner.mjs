8926fdf23e7d5d56a402fdb670baa016cabc6a9b
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-06-11 11:49:41 +0200

    feat: start tutorial big update

--- src/docs-generator/cleaner.mjs
+++
@@ -1,6 +0,0 @@
-import * as fs from "fs";
-
-fs.rmSync("docs/api", { recursive: true, force: true });
-fs.rmSync("docs/tutorials", { recursive: true, force: true });
-fs.rmSync("temp", { recursive: true, force: true });
-fs.mkdirSync("temp");
